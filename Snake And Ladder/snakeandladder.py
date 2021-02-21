a118b607afc19270e84100254755eab3f3b19787
Ashwin <ashwinn138@gmail.com>
2021-02-21 18:25:42 +0530

    First Python Project Completed

--- Snake And Ladder/snakeandladder.py
+++ Snake And Ladder/snakeandladder.py
@@ -49,10 +49,10 @@ class snakes_and_ladder():
 
                Rules:
               1. Initally both the players are at starting position i.e. 0. 
-                 Take it in turns to roll the dice. 
+                 Take in your turn to roll the dice. 
                  Move forward the number of spaces shown on the dice.
-              2. If you lands at the bottom of a ladder, you can move up to the top of the ladder.
-              3. If you lands on the head of a snake, you must slide down to the bottom of the snake.
+              2. If you land at the bottom of a ladder, you can move up to the top of the ladder.
+              3. If you land on the head of a snake, you must slide down to the bottom of the snake.
               4. The first player to get to the FINAL position is the winner.
               5. Hit enter to roll the dice.
 
